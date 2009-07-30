077a6b2cda9a98e529ce56b8491941a131b97858
Satish Chauhan <satishchauhan80@gmail.com>
2009-07-30 11:40:10 +0530

    modified 20090716082254_create_shipping_countries.rb to add store_country_id to shipping_countries

--- db/migrate/20090716082254_create_shipping_countries.rb
+++ db/migrate/20090716082254_create_shipping_countries.rb
@@ -1,12 +1,12 @@
 class CreateShippingCountries < ActiveRecord::Migration
   def self.up
     create_table :shipping_countries do |t|
-      t.references  :shipping_method, :country
+      t.references  :shipping_method, :store_country, :country
   
       t.timestamps
     end
-    add_index :shipping_countries, [:shipping_method_id, :country_id]
-    add_index :shipping_countries, :country_id
+    add_index :shipping_countries, [:shipping_method_id, :store_country_id], :name => 'index_shipping_countries_on_shipping_method_and_store_country'
+    add_index :shipping_countries, :store_country_id
   end
 
   def self.down
