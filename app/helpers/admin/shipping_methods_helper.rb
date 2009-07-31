a566f8779a217576eff24e864e6682b6d2aa67ce
Satish Chauhan <satishchauhan80@gmail.com>
2009-07-31 11:06:27 +0530

    renamed helper method options_for_shipping_country_select => options_for_store_country_select in admin namespace

--- app/helpers/admin/shipping_methods_helper.rb
+++ app/helpers/admin/shipping_methods_helper.rb
@@ -1,6 +1,6 @@
 module Admin::ShippingMethodsHelper
 
-  def options_for_shipping_country_select
+  def options_for_store_country_select
     @store.store_countries.collect{|store_country| ["#{ store_country.country.name } (#{ store_country.country.iso })", store_country.id]}
   end
 
