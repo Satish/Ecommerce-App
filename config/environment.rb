d745b70352728f3647e3781e902b9bcd30279ec7
Satish Chauhan <satishchauhan80@gmail.com>
2009-08-06 10:48:25 +0530

    added 'ruby-openid' to config.gem list in environment.rb so that it can be installed with rake gems:install

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "ruby-openid", :lib => "openid"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -45,6 +46,7 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 
+  PER_PAGE = 20
   require 'custome_methods'
 
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,7 +3,6 @@ BAD_REQUEST_ERROR_MESSAGE = "Sorry, We are unable to process your request..."
 FEEDS_DESCRIPTION_OPTIONS = ["Full", 'Short']
 ORDER_BY_OPTIONS = {"Arrange by A-Z" => "name ASC", "Arrange by Z-A" => "name DESC", "Price (Low-High)" => "price ASC", "Price (High-Low)" => "price DESC" }
 PER_PAGE_OPTIONS = [16, 40, 80, 120, 500]
-PER_PAGE = 20
 STATE_FOR_STATE_SELECT = ['United States', 'India', 'Australia', 'Spain', 'Uganda', 'France', 'German', 'Canada', 'Scotland', 'Ireland', 'Wales', 'United Kingdom' ]
 MAIL_AUTH = ['none', 'plain', 'login', 'cram_md5']
 SECURE_CONNECTION_TYPES = ['None','SSL','TLS']
